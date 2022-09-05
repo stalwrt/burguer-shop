fd5b4fa67218b7f07fa95848774486469ae853c5
_floreesleo <retr0.strwll@gmail.com>
2022-09-05 14:03:07 -0500

    Primera versión con el esqueleto del sitio y sin login

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Burger Shop | Inicio</title>
+
+    <!--| conexion con la hoja de estilos -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <nav class="navegacion">
+        logo
+            <a href="">Inicio</a>
+            <a href="">Alimentos</a>
+            <a href="">Postres</a>
+            <a href="">Bebidas</a>
+        </nav>
+    </header>
+
+    <div class="contenedor-principal">
+
+        <div class="producto">
+            <a href="#">
+                <h2>Producto</h2>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam autem temporibus quibusdam officia ipsa aut id delectus atque, itaque maiores.
+            </p>
+            <p>$56.00</p>
+            </a>
+        </div>
+
+        <div class="producto">
+            <a href="#">
+                <h2>Producto</h2>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam autem temporibus quibusdam officia ipsa aut id delectus atque, itaque maiores.
+            </p>
+            <p>$56.00</p>
+            </a>
+        </div>
+
+        <div class="producto">
+            <a href="#">
+                <h2>Producto</h2>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam autem temporibus quibusdam officia ipsa aut id delectus atque, itaque maiores.
+            </p>
+            <p>$56.00</p>
+            </a>
+        </div>
+
+        <div class="producto">
+            <a href="#">
+                <h2>Producto</h2>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam autem temporibus quibusdam officia ipsa aut id delectus atque, itaque maiores.
+            </p>
+            <p>$56.00</p>
+            </a>
+        </div>
+
+    </div>
+
+    <footer>
+        <p>
+            Derechos reservados.
+            <br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, ratione. Dolores repellendus dignissimos numquam, quo nostrum tenetur quaerat debitis rerum, odit dolor molestiae culpa nemo beatae pariatur! Veniam, dolorum illum?
+        </p>
+    </footer>
+</body>
+</html>
---
+++ php/conexion.php
@@ -0,0 +1,5 @@
+<?php
+
+$conexion = mysqli_connect("localhost","root","","burguershop") // Revisar si tengo contraseña en phpMyAdmin
+
+?>
