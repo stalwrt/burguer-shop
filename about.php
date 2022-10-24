f8e98c5dc7461def7a7039823afe7379bfe21089
_floreesleo <retr0.strwll@gmail.com>
2022-10-24 09:43:33 -0500

    Menu lateral en carrito.php y about.php

--- about.php
+++ about.php
@@ -16,15 +16,18 @@
 
 	<div class="main-contenedor">
 		<h1>Sobre nosotros</h1>
+		<h2>Misión</h2>
 		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, officiis ipsa? Dignissimos
-			placeat ex nostrum iure, incidunt nihil facere sed repellendus quis earum maiores. Ratione
-			quaerat qui cumque ducimus voluptas?
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sapiente obcaecati maxime veniam quo nobis ad laboriosam laborum iste iure quasi saepe quae aspernatur aliquam qui, pariatur explicabo reprehenderit assumenda.
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quis laudantium necessitatibus. Minima fuga laborum maxime illum illo beatae. Fugiat ipsam minus at modi soluta laboriosam vel dolorum quod officia.
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat libero est deserunt quod corporis, sint, sequi quis, sapiente ducimus dolorum eius! Totam id saepe quo cupiditate aspernatur similique voluptate repellendus!
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident enim nisi error, praesentium ullam quibusdam natus ex aliquid sint iure expedita odit labore, voluptatibus quam doloribus eveniet quod, officia vero!
 		</p>
 		<hr>
-		<br>
+		<h2>Visión</h2>
 		<p>
-			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi ut distinctio natus in fugiat voluptatibus rem explicabo ex cumque facilis beatae molestiae tenetur laudantium, ratione doloribus itaque libero. Voluptatibus, saepe!
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, quam ratione illum doloremque at minus similique assumenda itaque vitae repellat porro iure unde corrupti accusamus libero ipsam architecto fugit sequi?
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora consectetur nesciunt tenetur commodi voluptas facilis ratione dolore odio fuga incidunt cumque ab, odit possimus. Quidem placeat eaque veniam? Praesentium, autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est vero quasi sequi eaque atque aliquam porro neque impedit magni. Eos provident iusto fugit velit quisquam quos earum ratione minus.
 		</p>
 	</div>
 
--- carrito.php
+++ carrito.php
@@ -6,10 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bistro Online | Carrito</title>
+    <link rel="stylesheet" href="Assets/CSS/style.css">
 </head>
 
 <body>
 
+    <?php
+    include 'Templates/navbar.php';
+    ?>
+
+    <script type="text/javascript">
+        const toggleSidebar = () => document.body.classList.toggle('open');
+    </script>
 </body>
 
 </html>
